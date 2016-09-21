7a2751c3bb801880001c9c725b094280fdc1031a
kopopt <kopopt@gmail.com>
2016-09-21 11:02:25 -0700

    remove fm_config.py

--- py/fm_ops.py
+++ py/fm_ops.py
@@ -6,11 +6,11 @@ fm_ops = tf.load_op_library(os.path.dirname(os.path.realpath(__file__)) + '/../l
 
 @ops.RegisterGradient("FmScorer")
 def _fm_scorer_grad(op, pred_grad, reg_grad):
-  feature_ids = op.inputs[0]
-  feature_params = op.inputs[1]
-  feature_vals = op.inputs[2]
-  feature_poses = op.inputs[3]
-  factor_lambda = op.inputs[4]
-  bias_lambda = op.inputs[5]
-  with ops.control_dependencies([pred_grad.op, reg_grad.op]):
-    return None, fm_ops.fm_grad(feature_ids, feature_params, feature_vals, feature_poses, factor_lambda, bias_lambda, pred_grad, reg_grad), None, None, None, None
+    feature_ids = op.inputs[0]
+    feature_params = op.inputs[1]
+    feature_vals = op.inputs[2]
+    feature_poses = op.inputs[3]
+    factor_lambda = op.inputs[4]
+    bias_lambda = op.inputs[5]
+    with ops.control_dependencies([pred_grad.op, reg_grad.op]):
+        return None, fm_ops.fm_grad(feature_ids, feature_params, feature_vals, feature_poses, factor_lambda, bias_lambda, pred_grad, reg_grad), None, None, None, None
